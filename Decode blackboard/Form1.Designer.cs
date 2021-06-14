759345b55c7151027fe3a4854977af97ebd4598d
David Wis <davidwis000@gmail.com>
2021-06-14 13:06:46 -0300

    commit inicial

---
+++ Decode blackboard/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Decode_blackboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(24, 48);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(537, 196);
+            this.txtBox.TabIndex = 1;
+            this.txtBox.Text = "";
+            // 
+            // picture
+            // 
+            this.picture.Location = new System.Drawing.Point(24, 295);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(172, 97);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picture.TabIndex = 3;
+            this.picture.TabStop = false;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(589, 118);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(135, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Generar imagen";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(21, 22);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(223, 13);
+            this.lbl.TabIndex = 5;
+            this.lbl.Text = "Por favor ingrese el código en la caja de texto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.txtBox);
+            this.Name = "Form1";
+            this.Text = "Bugfix blackboard";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lbl;
+    }
+}
+
---
+++ Decode blackboard/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Web;
+
+/// <summary>
+/// Programa para generar fotos a partir del mathml del editor de blackboard mediante el servicio de wiris
+/// Creado con la intención de recuperar las respuestas del segundo parcial de la materia de Análisis Matemático I del ITBA 
+/// que no se visualizan correctamente por un bug del editor de ecuaciones de blackboard
+/// 
+/// Autor: David Wischñevsky
+/// </summary>
+namespace Decode_blackboard
+{
+
+    //https://www.wiris.net/demo/editor/
+    public partial class Form1 : Form
+    {
+        public const string url = @"https://www.wiris.net/demo/editor/render";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string rawText = txtBox.Text;
+            if (txtBox.Text.Length > 0)
+            {
+                // Primero se cambian los « y » por < y >, y los ¨ por ' y para que el servicio pueda leer el mathml
+                // Además, se reemplazan los signos + por su representacion para urls (%2B)
+                string newText = rawText.Replace("»", ">").Replace("«", "<").Replace("¨", "'").Replace("+", "%2B");
+
+                // Luego se reemplazan los caracteres especiales por sus respectivas encodificaciones en unicode
+                // Esto permite que caracteres como el ∞ se puedan cargar en la imagen
+                string cleanedString = Regex.Replace(newText, @"§#[0-9]+;", m => {
+                    string aux = m.Value.Replace("§", "&");
+                    return System.Net.WebUtility.HtmlDecode(aux);
+                });
+
+                try
+                {
+                    picture.Load($"{url}?mml={cleanedString}"); // Llamado al servicio de wiris.net para generar la foto
+                    Console.WriteLine($"{url}?mml={cleanedString}");
+                } 
+                catch (Exception)
+                {
+                    MessageBox.Show("La cadena de texto ingresada no es correcta o se produjo un error generando la imagen");
+                }
+            } else
+            {
+                MessageBox.Show("Por favor ingrese algún valor en la caja de texto");
+            }
+        }
+    }
+}
